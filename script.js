2ff9e55d3ce564d10b2bcf40d8e2f6b175759f47
malikhussain26 <hussain.am2697@gmail.com>
2024-09-12 12:39:19 -0500

    part 1 week 1

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 /**
  * 
  * 
+ * 
  */
 
 // variables
 const PI = 3.1415;
-const area = PI * radius * radius;
-const radius = 5;
-const plantSpace = 0.8;
-const areaStar = 20;
+let radius = 5;
+let area = PI * radius * radius;
+let plantSpace = 0.8;
+let plantStart = 20;
+
+// Plant Growth Control Flow
+let weeks = 1;
+let states = plantStart * Math.pow(2, weeks);
+let capacity = area / (plantSpace * plantStart);
+let percentage = states / capacity;
+
+// Pruned
+if (capacity > 80) {
+    console.log('pruned');
+} else if (capacity >= 50 && capacity <= 80) {
+    console.log('monitered')
+} else {
+    console.log('More room to grow plants')
+}
